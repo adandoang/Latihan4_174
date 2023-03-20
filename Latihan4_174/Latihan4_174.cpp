dc0dcb911b8bfcb16b5665ef26f915a86b2fce97
adandoang <akhdanjzif@gmail.com>
2023-03-20 15:02:18 +0700

    menentukan nilai numerik luas persegi, lingkaran, segitiga

--- Latihan4_174/Latihan4_174.cpp
+++ Latihan4_174/Latihan4_174.cpp
@@ -1,20 +1,36 @@
-// Latihan4_174.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+float LuasPersegi(float p, float l) {
+	return p * l;
+}
+float LuasLingkaran(float r) {
+	return 3.14 * r * r;
+}
+float LuasSegitiga(float a, float t) {
+	return 0.5 * a * t;
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	float panjang, lebar, jejari, alas, tinggi;
+	int pilihan;
+
+	do {
+		cout << "\n\n ================================" << endl;
+		cout << "============== MENU ==============" << endl;
+		cout << "==================================" << endl;
+		cout << "1. Luas Persegi " << endl;
+		cout << "2. Luas Lingkaran " << endl;
+		cout << "3. Luas Segitiga " << endl;
+		cout << "Exit " << endl;
+		cin >> pilihan;
+
+		switch (pilihan)
+		{
+		case 1:
+			cout << Masukan Panjang = 
+		}
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
